1dc1350b29f69f898798517771790081b776bcb6
Piotr Gabryjeluk <piotr.gabryjeluk@shortlist.co>
2020-12-29 19:56:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,104 @@
+# Pow
+
+Pow is a CLI tool to run useful commands easily configurable via Pow files.
+
+# Pow for you
+
+* Pow can replace a collection of useful command aliases and BASH scripts that almost everyone ends up keeping in their ~/bin/
+* Pow can help you make sure you never run `docker run` without `--rm`
+* Pow can help you with tasks you do from time to time but not often enough to remember the exact command line to use (and it's easier to share/version your pow files than your .bash_history for those relying on Ctrl-R)
+* Pow can be used to create wrappers for your workflow (you can wrap commands involving `aws`, `docker`, `git` or anything else)
+* Pow can be the way you share your scripts with others
+
+## Example
+
+Put the following in your `~/.pow_file.py`:
+
+```python
+Pow = {}
+
+def pow_avg(args):
+    """Calculate average of given integers"""
+    total = sum(map(int, args))
+    print(total / len(args))
+```
+
+Then you execute like this:
+
+```
+$ pow avg 1 2 3 4
+2.5
+```
+
+You can list all available commands:
+
+```
+$ pow
+
+Usage: pow [options] <command> [command parameters]
+
+  pow --help        Print pow's usage and available commands
+  pow -h            Alias for --help
+  pow avg           Calculate average of given integers
+  pow inspect-pow   Inspect loaded pow files
+
+```
+
+# Pow for your project
+
+* Pow aims to replace `npm run command`, `composer run-script command`, `./manage.py command` and the like -- in a framework independent fashion -- also sharing the entry point for parts done in different languages
+* Pow can be used to provide one interface for building, running, maintaining, and debugging your application
+* Pow offers a unified way to share scripts between developers
+
+## Example
+
+```
+$ pow
+
+Usage: pow [options] <command> [command parameters]
+
+  pow --help                 Print pow's usage and available commands
+  pow -h                     Alias for --help
+  pow extract-translations   Extract strings to translate from application files
+  pow generate-fake-data     Populate the database with randomly generated data
+  pow githook-precommit      Run checks before git commit
+  pow inspect-pow            Inspect loaded pow files
+  pow install-githooks       Install git hooks
+  pow lint                   Auto-format all Python and JavaScript files in this project
+  pow run-backend            Build and start backend in docker
+  pow run-frontend           Build and start frontend using node
+
+```
+
+In this example we see Pow used as a common way of discovering and running various commands for the project: extracting translations, linting the code, installing git hooks to help the team mates follow a process. The git hooks themselves are also executed through pow. The code for the commands might be split into multiple files like this:
+
+```
+pow_files/
+├── pow_backend.py
+├── pow_frontend.py
+├── pow_githooks.py
+└── pow_translations.py
+```
+
+Pow will load any file in `pow_files/` named `pow_*.py` so you can put together as many (or as few) commands in one Python file as you're comfortable with.
+
+# Dependencies and installation
+
+Pow is one Python 3 file with no further dependencies. Just drop it to your PATH, make executable, and you're done. You can also ship it in your project and invoke as `./pow command`.
+
+# Comparison to other tools
+
+Pow was inspired by the following tools and patterns:
+
+* Using a Makefile to store commands useful for building, running, maintaining, and debugging your application
+  * Pow file is easier to write than a Makefile
+  * Pow commands are available anywhere in the project, not only in the directory with the Makefile
+* Project scripts running through `yarn run`, `composer run-script`  (PHP), `./manage.py` (Django), etc
+  * Pow can be used if you don't have `yarn` or `composer`. Example: use Pow to *install* yarn or run yarn via Docker
+* Storing a ton of aliases in one's `.bashrc` or `.bash_aliases`
+  * Pow commands *can* be as simple as aliases, but they also *can* also have some logic
+  * You can have multiple Pow files and they will be automatically loaded if you put them to `pow_files` directory
+  * You can use whatever shell you want and your pow commands will work the same
+* A collection of useful short shell/Perl/Python/Ruby scripts in `~/bin`
+  * Multiple (as many or as few as desired) Pow commands can be put in the same file
+  * Common parts of your commands may be extracted to Pow plugins and reused
---
+++ src/pow.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python3
+
+import importlib.util
+import os
+import re
+import sys
+from pathlib import Path
+
+
+class PowEnv:
+    BASENAME = os.path.basename(sys.argv[0])
+    CWD = os.getcwd()
+    HOME = os.path.expanduser("~")
+    POW_FILE_GLOBS = [
+        "pow_file.py",
+        ".pow_file.py",
+        "pow_files/pow_*.py",
+        ".pow_files/pow_*.py",
+    ]
+    TERM = os.getenv("TERM")
+    UID = "{}".format(os.getuid())
+    USER = os.getenv("USER")
+
+
+class PowPlugins:
+    pass
+
+
+class PowUtils:
+    @staticmethod
+    def get_cwd_and_parents():
+        return [Path(PowEnv.CWD)] + list(Path(PowEnv.CWD).parents)
+
+    @staticmethod
+    def print_error(msg):
+        print(
+            "{}: {}".format(PowEnv.BASENAME, msg), file=sys.stderr,
+        )
+
+    @staticmethod
+    def print_usage():
+        usage = [
+            "\nUsage: {} [options] <command> [command parameters]\n".format(
+                PowEnv.BASENAME
+            )
+        ]
+        names = sorted(Pow.commands.keys())
+        longest_name_len = max((len(name) for name in names), default=0)
+        line_template = "  {} {{:{}}}   {{}}".format(PowEnv.BASENAME, longest_name_len)
+        for name in names:
+            usage.append(line_template.format(name, Pow.commands[name].__doc__ or ""))
+        print("\n".join(usage) + "\n", file=sys.stderr)
+
+    @staticmethod
+    def to_kabob_case(name):
+        return name.replace("_", "-")
+
+    @staticmethod
+    def to_snake_case(name):
+        return re.sub(r"(?<!^)(?=[A-Z])", "_", name).lower()
+
+
+class Pow:
+    # Helpers
+    env = PowEnv()
+    utils = PowUtils()
+
+    # For pow debugging
+    _command_origins = {}
+    _plugin_origins = {}
+    _pow_files = []
+
+    # pow_files fill these
+    commands = {}
+    plugins = PowPlugins()
+
+
+class PowDefaultCommands:
+    @staticmethod
+    def pow__h(args):
+        """Alias for --help"""
+        PowUtils.print_usage()
+
+    @staticmethod
+    def pow___help(args):
+        """Print pow's usage and available commands"""
+        PowUtils.print_usage()
+
+    @staticmethod
+    def pow_inspect_pow(args):
+        """Inspect loaded pow files"""
+        inventory = {}
+
+        for file in Pow._pow_files:
+            inventory[file] = {
+                "plugins": [],
+                "commands": [],
+            }
+
+        for plugin, origin in Pow._plugin_origins.items():
+            inventory[origin]["plugins"].append((plugin, getattr(Pow.plugins, plugin)))
+
+        for cmd, origin in Pow._command_origins.items():
+            inventory[origin]["commands"].append((cmd, Pow.commands[cmd]))
+
+        for file in Pow._pow_files:
+            plugins = sorted(inventory[file]["plugins"])
+            commands = sorted(inventory[file]["commands"])
+            print(
+                "* {}".format(
+                    "pow internal" if file == "__pow__" else "from {}".format(file)
+                )
+            )
+            print("  * plugins: {}".format(len(plugins)))
+            for plugin_name, _ in plugins:
+                print("    * {}".format(plugin_name))
+            print("  * commands: {}".format(len(commands)))
+            for cmd_name, _ in commands:
+                print("    * {}".format(cmd_name))
+            print("")
+
+
+def main(args):
+    register_pow_module(PowDefaultCommands, file_path="__pow__")
+    files_to_load = []
+    for path in reversed(PowUtils.get_cwd_and_parents()):
+        for glob in PowEnv.POW_FILE_GLOBS:
+            for pow_path in path.glob(glob):
+                if os.path.isfile(pow_path):
+                    files_to_load.append(pow_path.as_posix())
+    for file in files_to_load:
+        mod = load_pow_module(file)
+        register_pow_module(mod, file_path=file)
+
+    if not len(args):
+        PowUtils.print_usage()
+        return
+
+    cmd = PowUtils.to_kabob_case(args.pop(0))
+    fn = Pow.commands.get(cmd)
+    if fn is None:
+        PowUtils.print_error('command "{}" not found'.format(cmd))
+        return
+
+    return fn(args)
+
+
+def load_pow_module(path):
+    # load module
+    spec = importlib.util.spec_from_file_location(path, path)
+    mod = importlib.util.module_from_spec(spec)
+    try:
+        spec.loader.exec_module(mod)
+    except:
+        PowUtils.print_error("error loading pow file {}".format(path))
+        raise
+
+    mod.Pow = Pow
+    return mod
+
+
+def register_pow_module(mod, file_path):
+    # Register the file path itself
+    Pow._pow_files.append(file_path)
+
+    # Register commands
+    cmd_prefix = "pow_"
+    cmd_prefix_len = len(cmd_prefix)
+    for name in dir(mod):
+        if name.startswith(cmd_prefix):
+            fn = getattr(mod, name)
+            cmd_name = PowUtils.to_kabob_case(name[cmd_prefix_len:])
+            Pow.commands[cmd_name] = fn
+            Pow._command_origins[cmd_name] = file_path
+
+    # Register plugins
+    plugin_prefix = "PowPlugin"
+    plugin_prefix_len = len(plugin_prefix)
+    for name in dir(mod):
+        if name.startswith(plugin_prefix):
+            plugin = getattr(mod, name)()
+            plugin_name = PowUtils.to_snake_case(name[plugin_prefix_len:])
+            setattr(Pow.plugins, plugin_name, plugin)
+            Pow._plugin_origins[plugin_name] = file_path
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
