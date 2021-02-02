72670aaad856084528fd948e346f3042a1ea17d9
Piotr Gabryjeluk <piotr.gabryjeluk@shortlist.co>
2021-02-02 11:01:55 +0100

    Redo Windows distribution (also ship with Python 3.8.7)

--- npm/pow-wrapper.js
+++ npm/pow-wrapper.js
@@ -13,16 +13,15 @@ function main() {
   let cmd;
 
   if (process.platform === 'win32') {
-    cmd = path.join(package_dir, 'windows', 'pow.exe');
+    cmd = path.join(package_dir, 'windows', 'pow-runner.dist', 'pow-runner.exe');
   } else if (process.platform === 'linux') {
     cmd = path.join(package_dir, 'linux', 'pow-runner');
-    pow_args.unshift(path.join(package_dir, 'src', 'pow.py'));
   } else {
     // Bring your own Python
     cmd = 'python3';
-    pow_args.unshift(path.join(package_dir, 'src', 'pow.py'));
   }
 
+  pow_args.unshift(path.join(package_dir, 'src', 'pow.py'));
   cp = child_process.spawnSync(cmd, pow_args, {stdio: 'inherit'});
   if (cp.error) {
     return 127;
