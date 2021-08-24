484f54dc6c815262d460c187cfa4fbfc39a7827a
Piotr Gabryjeluk <piotr.gabryjeluk@shortlist.co>
2021-08-24 21:31:34 +0200

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,7 +1,8 @@
 print("test")
 print("test2")
-print("test3")
+print("test3")   # aaa
 print("test4")
-print("test5")
-print("test6")
-print("test7")
+print("test5") 
+print("test6") 
+print("test7") 
+print ("test8")
